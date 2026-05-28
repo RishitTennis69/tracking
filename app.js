b86f84779ec54544f1b4d95818962e0c6e8e3243
RishitTennis69 <135282797+RishitTennis69@users.noreply.github.com>
2026-05-28 12:03:49 -0700

    Updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const supabase = window.supabase.createClient(
+const supabase = window.supabase?.createClient?.(
   "https://0ec90b57d6e95fcbda19832f.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw"
-);
+) ?? null;
 
 const state = {
   config: null,
